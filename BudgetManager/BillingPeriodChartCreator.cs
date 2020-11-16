50c17897e0cb33796b3cfae02d3f061750159ca1
Adrian Frydmanski <adrian.frydmanski@nokia.com>
2020-11-16 10:30:12 +0100

    Billing period chart creator init + readme changes

---
+++ BudgetManager/BillingPeriodChartCreator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BudgetManager
+{
+    class BillingPeriodChartCreator
+    {
+        BillingPeriod period;
+        Grid grid;
+
+        public BillingPeriodChartCreator(BillingPeriod bp, Grid g)
+        {
+            period = bp;
+            grid = g;
+        }
+
+        public void Plot()
+        {
+
+        }
+    }
+}
---
+++ BudgetManager/README.md
@@ -0,0 +1,13 @@
+# BudgetManager
+
+Aplikacja umożliwiająca zapisywanie codziennych wydatków i wyświetlanie statystyk.
+
+## TODO
+
+- Widoki
+  - Widok wszystkich wydatków
+  - Wykres wypalenia
+  - Wykres podsumowania
+- Ustawienia
+  - Zmiana rozmiaru czcionki w ustawianiach
+  - Tryb jasny/szary
