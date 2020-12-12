cff3da5df967734f6712b99c1317ba85036014a1
Adrian Frydmanski <adrian.frydmanski@nokia.com>
2020-12-12 12:21:31 +0100

    Creating new billing period on startup

--- BudgetManager/App.xaml.cs
+++ BudgetManager/App.xaml.cs
@@ -14,16 +14,88 @@ namespace BudgetManager
     public partial class App : Application
     {
         private FilesHandler filesHandler;
-        protected void LoadData()
+        private void LoadData()
         {
             filesHandler = new FilesHandler();
             filesHandler.ReadData();
         }
 
+        private void CheckIfNewPeriodIsNeeded()
+        {
+            var lastPeriodEndDate = AppData.billingPeriods.Last().endDate;
+            var todayDate = DateTime.Today;
+            if (lastPeriodEndDate < todayDate)
+            {
+                var res = MessageBox.Show("Ostatni miesiąc się zakończył. Czy chcesz automatycznie utworzyć nowy?", "Koniec miesiąca!", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+                switch (res)
+                {
+                    case MessageBoxResult.Yes:
+                        CreateNextBillingPeriod();
+                        break;
+                    case MessageBoxResult.No:
+                        break;
+                    case MessageBoxResult.Cancel:
+                        Shutdown(0);
+                        break;
+                }
+            }
+        }
+
+        private void CreateNextBillingPeriod()
+        {
+            var lastPeriod = AppData.billingPeriods.Last();
+            var startDate = lastPeriod.endDate.AddDays(1);
+            var endDate = startDate.AddMonths(1);
+            var typicalEndDay = (AppData.settings.TypicalBeginningOfPeriod - 1) % 31 + 1;
+            var daysInMonth = DateTime.DaysInMonth(endDate.Year, endDate.Month);
+            if (typicalEndDay > daysInMonth)
+            {
+                typicalEndDay = daysInMonth;
+            }
+            endDate = new DateTime(endDate.Year, endDate.Month, typicalEndDay);
+            decimal[] incomesAndSavings = { decimal.Zero, decimal.Zero, decimal.Zero };
+            string[] types = { "przychód netto", "dodatkowy przychód", "planowane oszczędności" };
+            for (var i = 0; i < 3; i++)
+            {
+                var res = MessageBox.Show("Czy chcesz przenieść " + types[i] + "?", "Uwaga", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (res == MessageBoxResult.Yes)
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            incomesAndSavings[i] = lastPeriod.netIncome;
+                            break;
+                        case 1:
+                            incomesAndSavings[i] = lastPeriod.additionalIncome;
+                            break;
+                        case 2:
+                            incomesAndSavings[i] = lastPeriod.plannedSavings;
+                            break;
+                    }
+                }
+            }
+            
+            var period = new BillingPeriod()
+            {
+                startDate = startDate,
+                endDate = endDate,
+                netIncome = incomesAndSavings[0],
+                additionalIncome = incomesAndSavings[1],
+                plannedSavings = incomesAndSavings[2]
+            };
+            if (AppData.billingPeriods.Count > 0)
+            {
+                period.expenses = AppData.billingPeriods.Last().GetCopyOfMonthlyExpensesForNextPeriod();
+            }
+            AppData.billingPeriods.Add(period);
+            AppData.isDataChanged = true;
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            this.LoadData();
+            LoadData();
+            CheckIfNewPeriodIsNeeded();
         }
     }
 }
