589c7fd6ba29214df40ad7c10c5df73a75ec9ad9
frydmans <adrian.frydmanski@nokia.com>
2021-04-10 19:39:45 +0200

    Expenses list filtering

--- BudgetManager/Pages/ListPage.xaml.cs
+++ BudgetManager/Pages/ListPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,139 @@ namespace BudgetManager.Pages
     /// </summary>
     public partial class ListPage : Page
     {
+        public ExpenseCategory selectedCategory;
+
+        public DateTime? selectedDate;
+
         public ListPage()
         {
             InitializeComponent();
+            LoadCategories();
+            SetupDatePicker();
+            FillDataGridWithExpenses();
+        }
+
+        private void LoadCategories()
+        {
+            var categories = AppData.expenseCategories;
+            CategoriesComboBox.Items.Add("—");
+            CategoriesComboBox.SelectedIndex = 0;
+            foreach (var category in categories)
+            {
+                CategoriesComboBox.Items.Add(category);
+            }
+        }
+
+        private ExpenseCategory GetSelectedCategory()
+        {
+            var selectedCategoryIndex = CategoriesComboBox.SelectedIndex - 1;
+            return selectedCategoryIndex >= 0
+                ? AppData.expenseCategories.ElementAt(selectedCategoryIndex)
+                : null;
+        }
+
+        private void SetupDatePicker()
+        {
+            var selectedPeriod = AppData.billingPeriods?.ElementAt(AppData.currentPeriod);
+            var begin = selectedPeriod?.startDate;
+            var end = selectedPeriod?.endDate;
+            var today = DateTime.Today.Date;
+            ExpensesDatePicker.DisplayDateStart = begin;
+            ExpensesDatePicker.DisplayDateEnd = end;
+            ExpensesDatePicker.SelectedDate = selectedDate;
+            SetSelectedDate();
+        }
+
+        private void ClearSelectedDate()
+        {
+            ExpensesDatePicker.SelectedDate = null;
+            SetSelectedDate();
+        }
+
+        private void SetSelectedDate()
+        {
+            ExpensesDatePicker.SelectedDate = selectedDate;
+        }
+
+        private void ButtonClear_OnClick(object sender, RoutedEventArgs e)
+        {
+            ClearSelectedDate();
+        }
+
+        private void FillDataGridWithExpenses()
+        {
+            var expenses = AppData.billingPeriods?.ElementAt(AppData.currentPeriod).expenses;
+
+            var isCategorySelected = CategoriesComboBox.SelectedIndex != 0;
+            var category = GetSelectedCategory();
+            var date = ExpensesDatePicker.SelectedDate;
+            var isDateSelected = date != null;
+
+            ExpensesDataGrid.Items.Clear();
+            foreach (var expense in expenses)
+            {
+                if (((isCategorySelected && expense.category == category) || !isCategorySelected) &&
+                    ((isDateSelected && expense.date == date) || !isDateSelected))
+                {
+                    var expenseDataItem = new ExpenseDataItem(expense);
+                    ExpensesDataGrid.Items.Add(expenseDataItem);
+                }
+            }
+            SortWithDateAndCategory();
+        }
+
+        private void SortWithDateAndCategory()
+        {
+            ExpensesDataGrid.Items.SortDescriptions.Clear();
+            var dateColumn = ExpensesDataGrid.Columns[0];
+            var categoryColumn = ExpensesDataGrid.Columns[2];
+            var dateSortDescription = new SortDescription(dateColumn.SortMemberPath, ListSortDirection.Descending);
+            var categorySortDescription = new SortDescription(categoryColumn.SortMemberPath, ListSortDirection.Ascending);
+
+            ExpensesDataGrid.Items.SortDescriptions.Add(dateSortDescription);
+            ExpensesDataGrid.Items.SortDescriptions.Add(categorySortDescription);
+
+            foreach (var col in ExpensesDataGrid.Columns)
+            {
+                col.SortDirection = null;
+            }
+            dateColumn.SortDirection = ListSortDirection.Descending;
+            categoryColumn.SortDirection = ListSortDirection.Ascending;
+            
+            ExpensesDataGrid.Items.Refresh();
+        }
+
+        private void CategoriesComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedCategoryIndex = CategoriesComboBox.SelectedIndex - 1;
+            selectedCategory = GetSelectedCategory();
+            FillDataGridWithExpenses();
+        }
+
+        private void ExpensesDatePicker_OnSelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedDate = ExpensesDatePicker.SelectedDate;
+            FillDataGridWithExpenses();
+        }
+    }
+
+    class ExpenseDataItem
+    {
+        public DateTime date { get; set; }
+        public string value { get; set; }
+        public string comment { get; set; }
+        public string category { get; set; }
+        public Boolean monthlyExpense { get; set; }
+        private Expense originalExpense;
+
+        public ExpenseDataItem(Expense exp)
+        {
+            date = exp.date;
+            value = exp.value.ToString("F");
+            comment = exp.comment;
+            category = exp.category.name;
+            monthlyExpense = exp.monthlyExpense;
+            originalExpense = exp;
         }
     }
 }
