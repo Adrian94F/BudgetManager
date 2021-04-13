204edc184779f0debc955c5fd236bf524eed0dd4
frydmans <adrian.frydmanski@nokia.com>
2021-04-13 11:16:58 +0200

    annotations fix

--- BudgetManager/Pages/ListPage.xaml.cs
+++ BudgetManager/Pages/ListPage.xaml.cs
@@ -15,7 +15,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using BudgetManager.Annotations;
 using ModernWpf.Controls;
 using ModernWpf.Controls.Primitives;
 using Frame = System.Windows.Controls.Frame;
--- BudgetManager/User controls/ExpensesTable.xaml.cs
+++ BudgetManager/User controls/ExpensesTable.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using BudgetManager.Annotations;
 using BudgetManager.Pages;
 using ModernWpf.Controls;
 using Frame = System.Windows.Controls.Frame;
