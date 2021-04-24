0e4b5bd21a853e34706a9ccf173b301e9a61c9e3
Adrian94F <adrian.frydmanski@hotmail.com>
2021-04-24 19:16:46 +0200

    Added categories to settings

---
+++ BudgetManager/User controls/ExpenseCategories.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BudgetManager.Pages;
+
+namespace BudgetManager.User_controls
+{
+    /// <summary>
+    /// Interaction logic for ExpenseCategories.xaml
+    /// </summary>
+    public partial class ExpenseCategories : UserControl
+    {
+        public ExpenseCategories()
+        {
+            InitializeComponent();
+            LoadCategories();
+        }
+
+        private void LoadCategories()
+        {
+            var categoriesCollection = new ObservableCollection<CategoryDataItem>();
+            
+            foreach (var category in AppData.expenseCategories)
+            {
+                var categoryDataItem = new CategoryDataItem(category);
+                categoriesCollection.Add(categoryDataItem);
+            }
+
+            CategoriesDataGrid.ItemsSource = categoriesCollection;
+        }
+    }
+
+    class CategoryDataItem
+    {
+        public string name { get; set; }
+        public ExpenseCategory originalCategory;
+
+        public CategoryDataItem(ExpenseCategory category)
+        {
+            name = category.name;
+            originalCategory = category;
+        }
+    }
+}
