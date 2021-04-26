c3d3c932315920c67205fbafcdfd8bbb441cdba0
Adrian94F <adrian.frydmanski@hotmail.com>
2021-04-26 13:09:17 +0200

    Empty data placeholder + new directory for images

---
+++ BudgetManager/User controls/EmptyAppDataPlaceholder.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BudgetManager.User_controls
+{
+    /// <summary>
+    /// Interaction logic for EmptyAppDataPlaceholder.xaml
+    /// </summary>
+    public partial class EmptyAppDataPlaceholder : UserControl
+    {
+        public EmptyAppDataPlaceholder()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckIfNeeded()
+        {
+            if (AppData.IsNotEmpty())
+            {
+                this.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                this.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
